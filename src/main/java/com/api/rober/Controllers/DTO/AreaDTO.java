ef3d5a7965e6a24cbd0411e0486288d5584be3fe
juniorDeveloper8 <be6323787@gmail.com>
2024-04-21 16:31:45 -0500

    creacion de metodos del controlador Area

---
+++ src/main/java/com/api/rober/Controllers/DTO/AreaDTO.java
@@ -0,0 +1,24 @@
+package com.api.rober.Controllers.DTO;
+
+import com.api.rober.Entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AreaDTO {
+    //private List<Product> productList = new ArrayList<>();
+
+    private Integer id_ar;
+    private String name;
+    private String des;
+
+    private List<User> userList = new ArrayList<>();
+}
